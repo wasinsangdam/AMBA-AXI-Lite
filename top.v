1fca3c74d978a519c2323045f63235472ad86732
seungjae <seungjyout@gmail.com>
2023-09-22 18:03:01 +0900

    add comments

--- top.v
+++ top.v
@@ -9,28 +9,37 @@ module top;
     reg                                     ACLK    ;
     reg                                     ARESET  ;
 
+    // Write Address Channel (AW)
     wire    [AXI_ADDR_WIDTH-1 : 0]          AWADDR  ;
     wire                                    AWVALID ;
     wire                                    AWREADY ;
 
+    // Write Data Channel (W)
     wire    [AXI_DATA_WIDTH-1 : 0]          WDATA   ;
     wire                                    WVALID  ;
     wire                                    WREADY  ;
     wire    [AXI_DATA_WIDTH/8-1 : 0]        WSTRB   ;
 
+    // Write Response Channel (B)
     wire    [1 : 0]                         BRESP   ;
     wire                                    BVALID  ;
     wire                                    BREADY  ;
 
+    // Read Address Channel (AR)
     wire    [AXI_ADDR_WIDTH-1 : 0]          ARADDR  ;
     wire                                    ARVALID ;
     wire                                    ARREADY ;
 
+    // Read Data Channel (R)
     wire    [AXI_DATA_WIDTH-1 : 0]          RDATA   ;
     wire    [1 : 0]                         RRESP   ;
     wire                                    RVALID  ;
     wire                                    RREADY  ;
 
+
+    //===================//
+    //  Master Instance  //
+    //===================//
     m_axil_register_bfm # (
         .M_AXI_ADDR_WIDTH(AXI_ADDR_WIDTH),
         .M_AXI_DATA_WIDTH(AXI_DATA_WIDTH)
@@ -62,6 +71,9 @@ module top;
         .RREADY     (RREADY )
     );
 
+    //==================//
+    //  Slave Instance  //
+    //==================//
     s_axil_register # (
         .S_AXI_ADDR_WIDTH(AXI_ADDR_WIDTH),
         .S_AXI_DATA_WIDTH(AXI_DATA_WIDTH)
@@ -93,6 +105,9 @@ module top;
         .RREADY     (RREADY )
     );
 
+    //=========================//
+    //  Initial value & Reset  //
+    //=========================//
     initial begin
         ACLK    = 1'b0;
         ARESET  = 1'b0;
@@ -104,8 +119,15 @@ module top;
         ARESET  = 1'b0;
     end
 
+    //=========================//
+    //  Create Clock (100MHz)  //
+    //=========================//
     always #5 ACLK = ~ACLK;
 
+
+    //=================//
+    //  Main function  //
+    //=================//
     initial begin
         wait(ARESET == 1'b0);
         wait(ARESET == 1'b1);
@@ -117,6 +139,9 @@ module top;
         $finish;
     end
 
+    //=============//
+    //  Dump file  //
+    //=============//
     initial begin
         $dumpfile("wave.vcd");
         $dumpvars(0);
